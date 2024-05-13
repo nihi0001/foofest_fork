0739ac936ae447a77da778ad3b3a133e2696935e
Nina <nihi0001@kea.dk>
2024-05-13 10:50:11 +0200

    test

--- src/app/lineup/page.jsx
+++ src/app/lineup/page.jsx
@@ -1,13 +1,31 @@
 // line up
 
-import React from 'react'
+import {useState, useEffect } from "react";
 
-function page() {
+
+async function getArtist(){
+  const res = await fetch ("https://yielding-cooperative-tarsal.glitch.me/bands");
+  const artists = await res.json();
+  defineArtist(artists);
+}
+getArtists();
+
+
+
+//export default async function Page({searchParams}) {
+ /* const params = new URLSearchParams(searchParams);
+  const response = await fetch (
+    `https://yielding-cooperative-tarsal.glitch.me/bands${params.toString()}`
+  );
+
+  const data = await response.json();
+
+
   return (
     <div>
+     <h1 className='text-White'>{data.name}</h1> 
+     <h1 className='text-White'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda libero suscipit distinctio commodi possimus cupiditate quasi ducimus provident facere dicta obcaecati, placeat et corporis adipisci, dolores, ea ut? Neque, cupiditate.</h1>
       
     </div>
   )
-}
-
-export default page
+}*/
