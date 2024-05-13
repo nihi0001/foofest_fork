b994ff3d885aa4fc6f45fc3d32906ad743fa84ea
Nina <nihi0001@kea.dk>
2024-05-13 14:26:01 +0200

    bands

--- src/app/lineup/page.jsx
+++ src/app/lineup/page.jsx
@@ -1,31 +1,34 @@
 // line up
 
-import {useState, useEffect } from "react";
+//import {useState, useEffect } from "react";
 
 
-async function getArtist(){
-  const res = await fetch ("https://yielding-cooperative-tarsal.glitch.me/bands");
-  const artists = await res.json();
+//async function getArtist(){
+// const res = await fetch ("https://yielding-cooperative-tarsal.glitch.me/bands");
+/*   const artists = await res.json();
   defineArtist(artists);
 }
-getArtists();
+getArtists(); */
 
 
 
-//export default async function Page({searchParams}) {
- /* const params = new URLSearchParams(searchParams);
-  const response = await fetch (
-    `https://yielding-cooperative-tarsal.glitch.me/bands${params.toString()}`
+export default async function Page({ searchParams }) {
+  const response = await fetch(
+    `https://yielding-cooperative-tarsal.glitch.me/bands`
   );
 
   const data = await response.json();
-
+  console.log(data)
 
   return (
     <div>
-     <h1 className='text-White'>{data.name}</h1> 
-     <h1 className='text-White'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda libero suscipit distinctio commodi possimus cupiditate quasi ducimus provident facere dicta obcaecati, placeat et corporis adipisci, dolores, ea ut? Neque, cupiditate.</h1>
-      
+      {data.map(band => (
+
+        <h1 className='text-White'>{band.name}</h1>
+      ))}
+
+      <h1 className='text-White'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda libero suscipit distinctio commodi possimus cupiditate quasi ducimus provident facere dicta obcaecati, placeat et corporis adipisci, dolores, ea ut? Neque, cupiditate.</h1>
+
     </div>
   )
-}*/
+}
