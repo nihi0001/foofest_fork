97ddec75df23d2ac07e902418cfe788cb3e1db7b
Nina <nihi0001@kea.dk>
2024-05-13 12:56:21 +0200

    outfit

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,7 +2,7 @@ import { Outfit } from "next/font/google";
 import "./globals.css";
 
 
-const inter = Inter({ subsets: ["latin"] });
+const outfit = Outfit({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
