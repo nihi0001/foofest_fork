25e4e82668d9651157144a6dbf530344637edd89
Nina <nihi0001@kea.dk>
2024-05-14 21:23:42 +0200

    styling af single page

--- src/app/lineup/[slug]/page.jsx
+++ src/app/lineup/[slug]/page.jsx
@@ -1,6 +1,8 @@
 // single view artist/band
 
 import React from 'react'
+import Image from "next/image";
+
 
 
 export const metadata = {
@@ -8,30 +10,41 @@ export const metadata = {
   description: "",
 };
 
- export async function generateStaticParams(){
-  const res = await fetch ("https://yielding-cooperative-tarsal.glitch.me/bands/");
+export async function generateStaticParams() {
+  const res = await fetch("https://yielding-cooperative-tarsal.glitch.me/bands/");
   const pages = await res.json()
 
   const paths = pages.map((page) => {
-    return {slug: page.slug};
+    return { slug: page.slug };
   });
 
   return paths;
-} 
+}
 
 
-export default async function Page({params}) {
-    const {slug} = params;
-    const url = `https://yielding-cooperative-tarsal.glitch.me/bands/${slug}`;
-    const res = await fetch(url);
+export default async function Page({ params }) {
+  const { slug } = params;
+  const url = `https://yielding-cooperative-tarsal.glitch.me/bands/${slug}`;
+  const res = await fetch(url);
   const artist = await res.json()
   console.log(artist)
   return (
-    <div className='max-w-xl mx-auto'>
-        <h1 className='text-White'>{artist.name}</h1>
-        <p className='bg-Hotpink text-Navyblue'>Friday, July 3rd, 8:15 PM, Main stage</p>
-        <p className='text-White'>{artist.bio}</p>
-      
+    <div className='max-w-2xl mx-auto mt-12 mb-12'>
+      <Image
+        alt="Artist presentation"
+        src={`https://yielding-cooperative-tarsal.glitch.me/logos/${artist.logo}`}
+        width={350}
+        height={350}
+        className='mx-auto'
+      />
+      <h2 className='text-White text-center text-4xl mt-5'>{artist.name}</h2>
+      <h3 className='text-White text-center'>{artist.genre}</h3>
+      <p className='bg-Hotpink text-Navyblue mt-5 p-2 text-center font-medium'>{artist.members}</p>
+
+      <p className='text-White mt-5 text-md leading-6 font-normal	'>{artist.bio}</p>
+
+      <p className='text-White mt-5 text-md leading-6'>{artist.logoCredits}</p>
+
     </div>
   );
 }
