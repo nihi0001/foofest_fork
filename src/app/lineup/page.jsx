721c76c3bb6c2094ef873e6b0657cf3bc3c332c0
Nina <nihi0001@kea.dk>
2024-05-15 09:35:18 +0200

    styling

--- src/app/lineup/page.jsx
+++ src/app/lineup/page.jsx
@@ -25,15 +25,34 @@ export default async function Page({ searchParams }) {
   const data = await response.json();
   console.log(data)
 
-  return (
-    <div>
-      {data.map(band => (
 
-        <h1 key={band.name} className='text-White'>{band.name}</h1>
-      ))}
+  // hvad siden returnere
 
-      <h1 className='text-White'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda libero suscipit distinctio commodi possimus cupiditate quasi ducimus provident facere dicta obcaecati, placeat et corporis adipisci, dolores, ea ut? Neque, cupiditate.</h1>
+  return <section>
+    <h1 className="text-White text-6xl text-center mb-5 p-16">LINE UP</h1>
 
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 max-w-4xl mx-auto">
+    {data.map((band) => {
+      return <div key={band.name}>
+        <Link href={`/lineup/${band.slug}`} prefetch={false}>
+          <div>
+
+            <div className="border-2 border-Hotpink p-2 rounded-sm">
+              <Image
+                alt="Artist presentation"
+                src={`https://yielding-cooperative-tarsal.glitch.me/logos/${band.logo}`}
+                width={350}
+                height={350}
+              />
+              <h2 className='text-White mt-5 text-lg font-light'>{band.genre}</h2>
+              <h1 className='text-Hotpink text-3xl font-semibold'>{band.name}</h1>
+
+            </div>
+          </div>
+        </Link>
+      </div>
+    })}
     </div>
-  )
+  </section>
+
 }
