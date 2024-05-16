dabad722ffa7805b112f670a30d15d7feb65b20c
Frederikke <frny0001@stud.kea.dk>
2024-05-16 11:20:43 +0200

    test

--- src/app/schedule/page.jsx
+++ src/app/schedule/page.jsx
@@ -1,3 +1,4 @@
+
 import React from "react";
 import { Bowlby_One } from "next/font/google";
 import Image from "next/image";
@@ -8,9 +9,9 @@ const BowlbyOne = Bowlby_One({
   display: "swap",
 });
 
-export const metadata = {
-  title: "FooFest | Program",
-};
+// export const metadata = {
+// title: "FooFest | Program",
+//}; 
 
 
 export default async function SchedulePage() {
@@ -27,30 +28,28 @@ export default async function SchedulePage() {
 
   const scenes = ["Midgard", "Jotunheim", "Vanaheim"];
   const weekdays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
-
+console.log(schedule.Midgard);
   //mapper image fra /bands ind og scener + ugedage fra /schedule
   return (
   <section>
     <h1 className="text-White text-6xl text-center mb-5 p-16">SCHEDULE</h1>
     <div>
-    {bands.map((band) => {
+    {bands.map((band, schedule) => {
       return <div key={band.name}>
        
-          <div>
-
+      <div>
+        <p className="text-White">{schedule["start"]}</p>
           <Image
                 alt="Artist presentation"
                 src={`https://yielding-cooperative-tarsal.glitch.me/logos/${band.logo}`}
                 width={350}
                 height={350}
               />
-              <p>{schedule.scene}</p>
-          </div>
-    
       </div>
 
-      
-
+          
+          
+      </div>
     })}
     </div>
   </section>
