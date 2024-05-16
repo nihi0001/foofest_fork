6cec9a7b534ba85bc8618d2072bbb7c1b281c969
Nina <nihi0001@kea.dk>
2024-05-16 22:27:17 +0200

    knap

--- src/app/components/LpBanner.jsx
+++ src/app/components/LpBanner.jsx
@@ -16,9 +16,7 @@ function LpBanner() {
         <div className="text-center">
           <h1 className={` text-White text-8xl ${BowlbyOne.className}`}>FOO FESTIVAL</h1>
           <p className={`text-White text-6xl mb-6 ${BowlbyOne.className}`}>2024</p>
-          <Link className="bg-Hotpink text-White text-base p-3 px-6 rounded-full" href={`/lineup`} prefetch={false}>
-            LINE UP
-          </Link>
+          <img src="/buttonpil.png" alt="" className="h-16 w-16 mx-auto mt-24"/>
         </div>
     </div>
     </div>
