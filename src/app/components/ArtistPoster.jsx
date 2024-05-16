aaeeddbd1a380e809621cc8b3fcd15c2ed63c29b
Nina <nihi0001@kea.dk>
2024-05-16 12:10:39 +0200

    plakat

--- src/app/components/ArtistPoster.jsx
+++ src/app/components/ArtistPoster.jsx
@@ -1,4 +1,6 @@
+"use client"
 import React from 'react'
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import { Bowlby_One } from "next/font/google";
  
@@ -8,47 +10,48 @@ const BowlbyOne = Bowlby_One({
   display: "swap",
 });
 
-
 function ArtistPoster() {
-    return (
-        <div className='flex justify-center items-center h-screen'>
-            <div className='text-White'>
-            <h2 className={`text-center mb-5 text-4xl ${BowlbyOne.className}`}>PROGRAM</h2>
+    const [names, setNames] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+  
+    useEffect(() => {
+      fetch("https://yielding-cooperative-tarsal.glitch.me/bands")
+        .then((res) => res.json())
+        .then((data) => {
+          setNames(data);
+          setIsLoading(false);
+        });
+    }, []);
 
-                <div className='flex mb-2 gap-6 text-4xl text-center'>
-                    <section className=''>Justin Bieber</section>
-                    <section>SZA</section>
-                    <section>Burna Boy</section>
-                    <section>Tems</section>
+    return(
+        <>
+        <div className='max-w-6xl mx-auto mt-12 mb-20 border-2 border-Hotpink p-12 rounded-xl'>
+        <h1 className={` text-White text-center text-5xl mb-12 ${BowlbyOne.className}`}>LINE UP</h1>
+        
+        <ol
+            className={`flex flex-wrap gap-5 items-baseline justify-center uppercase`}>
+            {names.slice(0, 30).map((band, index) => (
+              <Link href={`/lineup/${band.slug}`} key={band.slug}>
+                <li
+                  key={band.slug}
+                  className={
+                    index < 4
+                      ? "text-4xl cursor-pointer text-White hover:text-Hotpink hover:scale-105 transition duration-300 ease-in-out font-bold"
+                      : index < 16
+                      ? "text-2xl cursor-pointer text-White hover:text-Hotpink hover:scale-105 transition duration-300 ease-in-out font-bold"
+                      : "text-md cursor-pointer	 text-White hover:text-Hotpink hover:scale-105 transition duration-300 ease-in-out font-bold"
+                  }
+                >
+                  {band.name}
+                </li>
+              </Link>
+            ))}
 
-                </div>
+          </ol>
+          </div>
 
-                <div className='flex mb-2 gap-6 text-2xl text-center'>
-                    <section>Justin Bieber</section>
-                    <section>SZA</section>
-                    <section>Burna Boy</section>
-                    <section>Tems</section>
-                </div>
-
-                <div className='flex mb-2 gap-6 text-xl text-center'>
-                    <section>Justin Bieber</section>
-                    <section>SZA</section>
-                    <section>Burna Boy</section>
-                    <section>Tems</section>
-                </div>
-
-                <div className='flex mb-2 gap-6 text-lg text-center'>
-                    <section>Justin Bieber</section>
-                    <section>SZA</section>
-                    <section>Burna Boy</section>
-                    <section>Tems</section>
-                </div>
-
-                {/* <Link className="bg-Hotpink text-white">SCHEDULE</Link>   */}
-
-            </div>
-        </div>
+        </>
     )
-}
+}  
 
 export default ArtistPoster
