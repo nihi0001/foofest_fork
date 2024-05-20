9c987b8c4ec708ee8e2908694603b8840b4c788d
Nina <nihi0001@kea.dk>
2024-05-20 15:40:55 +0200

    font

--- src/app/components/VipTicket.jsx
+++ src/app/components/VipTicket.jsx
@@ -4,7 +4,7 @@ import BuyTicket from './BuyTicket'
 export default function VipTicket() {
   return (
     <div className='grid gap-4 bg-Darkblue border-solid border-Hotpink border-2 p-10 rounded-2xl w-72'>
-      <h2 className='text-2xl text-start'>
+      <h2 className='text-3xl text-start font-normal'>
         <span className='text-Hotpink font-bold'>VIP </span>
         TICKET
       </h2>
