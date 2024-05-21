5916b42309141d62e4ec97f4b007cd3bd062157f
Frederikke <frny0001@stud.kea.dk>
2024-05-21 09:00:11 +0200

    formupdates

--- src/app/components/GuestForm.jsx
+++ src/app/components/GuestForm.jsx
@@ -4,6 +4,7 @@
 import { useRouter } from "next/router"
 import { useState } from "react"
 
+
 export default function GuestForm() {
 
 
@@ -43,10 +44,10 @@ body: JSON.stringify(info)
 
    
 
- <form onSubmit={handleSubmit} className="w-1/2">
- <h2>Guest 1</h2>
+ <form onSubmit={handleSubmit} className="flex flex-col m-6">
+ <h2 className="mb-4">Guest 1</h2>
  <label>
-  <span>Country:</span>
+  <span>Country</span>
 <select className="rounded-md p-3 border-2 w-60  hover:bg-White active:bg-White focus:outline-none focus:ring focus:ring-Hotpink text-Darkblue"
   onChange={(e) => setCountry(e.target.value)}
   value={country} 
@@ -56,79 +57,91 @@ body: JSON.stringify(info)
   <option value="Norway">Norway</option>
 </select>
 </label>
+<div>
+
 <label>
-  <span>First name:</span>
+  <span>First name</span>
   <input className="rounded-md p-2 border-2  hover:bg-White active:bg-White focus:outline-none focus:ring focus:ring-Hotpink text-Darkblue" 
   required
   type="text"
   placeholder="Jane"
+  name="firstname"
   onChange={(e) => setFirstName(e.target.value)}
   value={firstname} 
   />
 </label>
+
+
 <label>
-  <span>Last name:</span>
+  <span>Last name</span>
   <input className="rounded-md p-2 border-2 hover:bg-White active:bg-White focus:outline-none focus:ring focus:ring-Hotpink text-Darkblue" 
   required
   type="text"
   placeholder="Doe"
+  name="lastname"
   onChange={(e) => setLastName(e.target.value)}
   value={lastname} 
   />
 </label>
+</div>
 <label>
-  <span>Email:</span>
+  <span>Email</span>
   <input className="rounded-md p-2 border-2 hover:bg-White active:bg-White focus:outline-none focus:ring focus:ring-Hotpink text-Darkblue" 
   required
   type="Email"
   placeholder="example@gmail.com"
+  name="email"
   onChange={(e) => setEmail(e.target.value)}
   value={useremail} 
   />
 </label>
 <label>
-  <span>Postal code:</span>
+  <span>Postal code</span>
   <input className="rounded-md p-2 border-2  hover:bg-White active:bg-White focus:outline-none focus:ring focus:ring-Hotpink text-Darkblue" 
   required
   type="numeric"
   placeholder="2950"
+  name="postal"
+  maxLength="4"
   onChange={(e) => setPostal(e.target.value)}
   value={postalcode} 
   />
 </label>
 <label>
-  <span>Address:</span>
+  <span>Address</span>
   <input className="rounded-md p-2 border-2  hover:bg-White active:bg-White focus:outline-none focus:ring focus:ring-Hotpink text-Darkblue" 
   required
   type="text"
   placeholder="Address"
+  name="address"
   onChange={(e) => setAddress(e.target.value)}
   value={address} 
   />
 </label>
 <label>
-  <span>City:</span>
+  <span>City</span>
   <input className="rounded-md p-2 border-2 hover:bg-White active:bg-White focus:outline-none focus:ring focus:ring-Hotpink text-Darkblue" 
   required
   type="text"
   placeholder="Paris"
+  name="city"
   onChange={(e) => setCity(e.target.value)}
   value={city} 
   />
 </label>
 <label>
-  <span>Phone number:</span>
+  <span>Phone number</span>
   <input className="rounded-md p-2 border-2  hover:bg-White active:bg-White focus:outline-none focus:ring focus:ring-Hotpink  text-Darkblue" 
   required
   type="numeric"
   placeholder="+45 12 34 56 78"
-  size="11" 
+  name="phonenumber"
   onChange={(e) => setPhone(e.target.value)}
   value={phonenumber} 
   />
 </label>
 
-<button className="text-center text-White p-4 mt-8 rounded-lg bg-Hotpink hover:bg-Lightoink w-80"
+<button className="text-center text-White p-2 mt-6 rounded-lg bg-Hotpink hover:bg-Lightoink w-60"
 disabled={isLoading}
 >
 {isLoading && <span>Add</span>}
