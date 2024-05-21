37ec3ce1c3750612413f13e1ab853717b4dd93a6
Nina <nihi0001@kea.dk>
2024-05-21 09:55:11 +0200

    LINEUP SIDE

--- src/app/lineup/page.jsx
+++ src/app/lineup/page.jsx
@@ -1,11 +1,22 @@
 "use client"
 import React from 'react'
 import Artistcard from "@/app/components/Artistcard";
+import LineupBanner from '../components/LineupBanner';
+import { Bowlby_One } from 'next/font/google';
+
+const BowlbyOne = Bowlby_One({
+  subsets: ['latin'],
+  weight: '400',
+  display: 'swap',
+});
 
 
 function page() {
   return (
     <div>
+      <LineupBanner/>
+      <h1 className={`text-White text-5xl -mt-5 text-center mb-8 md:text-8xl md:-mt-20 md:p-7 ${BowlbyOne.className}`}>LINE UP</h1>
+
       <Artistcard />
       
     </div>
@@ -13,3 +24,4 @@ function page() {
 }
 
 export default page
+
