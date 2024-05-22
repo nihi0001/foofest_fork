65dae95da86514351e3f9eb220623233ad689bce
Frederikke <frny0001@stud.kea.dk>
2024-05-22 16:46:42 +0200

    stylingorderconfirmation

--- src/app/booking/orderConfirmation/page.jsx
+++ src/app/booking/orderConfirmation/page.jsx
@@ -1,16 +1,26 @@
 // order conformation - tak for dit køb
 import Link from "next/link"
+import { Bowlby_One } from "next/font/google";
+
+const BowlbyOne = Bowlby_One({
+  subsets: ["latin"],
+  weight: "400",
+  display: "swap",
+});
 
 function page() {
   return (
-    <div className="text-White bg-Darkblue p-16 m-20 mt-20 rounded-md border-2 border-Hotpink">
-      <h1 className="font-bold text-center text-4xl mb-3">THANK <span className='text-Hotpink'>YOU</span> FOR YOUR ORDER</h1>
-      <h2 className="text-center text-xl mb-7">We have sent an order confirmation to your email</h2>
+    <section>
+    <div className="text-White bg-Darkblue p-16 m-20 rounded-md border-2 border-Hotpink">
+    
+      <h1 className={`font-bold text-center mb-3 text-2xl md:text-4xl ${BowlbyOne.className}`}>THANK <span className='text-Hotpink'>YOU</span> FOR YOUR ORDER</h1>
+      <h2 className="text-lg text-center md:text-xl mb-7">We have sent an order confirmation along with your tickets to your email!</h2>
       <div className='flex items-center justify-center'>
-      <Link className=" text-center text-Darkblue p-4 mt-8 rounded-3xl bg-Lightpink hover:bg-Hotpink text-white w-80" href={`/`}>
+      <Link className=" text-sm md:text-lg text-center text-White p-4 mt-8 rounded-2xl bg-Hotpink hover:bg-Lightpink text-white max-w-auto" href={`/`}>
         REDIRECT TO FRONT PAGE</Link>
       </div>
     </div>
+    </section>
   )
 }
 
--- src/app/components/GuestForm.jsx
+++ src/app/components/GuestForm.jsx
@@ -1,6 +1,21 @@
 "use client"
-import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
+export async function getInfo() {
+  let headersList = {
+    "Accept": "*/*",
+    "apikey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV2Zm1pZmhrYmFtd21hZ25xaHJpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYxMjUwNzUsImV4cCI6MjAzMTcwMTA3NX0.4uzJxDYPdBPq_wYwJAZgUgePZwcAkGYMD04j2J296Vk"
+  };
+
+  let response = await fetch("https://uvfmifhkbamwmagnqhri.supabase.co/rest/v1/FooFestBooking", { 
+    method: "GET",
+    headers: headersList
+  });
+
+  let data = await response.text();
+  console.log(data);
+}
 
 export default function GuestForm() {
   const [firstname, setFirstName] = useState('');
@@ -20,33 +35,33 @@ export default function GuestForm() {
     setIsLoading(true);
 
     const info = {
-      firstname, 
-      lastname, 
-      country, 
-      useremail, 
-      postalcode, 
-      address, 
-      city, 
+      firstname,
+      lastname,
+      country,
+      useremail,
+      postalcode,
+      address,
+      city,
       phonenumber
     };
 
-    const bodyContent = JSON.stringify(info);
+    let bodyContent = JSON.stringify(info);
     let headersList = {
       "Accept": "*/*",
       "apikey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV2Zm1pZmhrYmFtd21hZ25xaHJpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYxMjUwNzUsImV4cCI6MjAzMTcwMTA3NX0.4uzJxDYPdBPq_wYwJAZgUgePZwcAkGYMD04j2J296Vk",
       "prefer": "return=representation",
       "Content-Type": "application/json"
-     }
+    };
 
     try {
-      const response = await fetch("https://uvfmifhkbamwmagnqhri.supabase.co/rest/v1/FooFestBooking", { 
+      let response = await fetch("https://uvfmifhkbamwmagnqhri.supabase.co/rest/v1/FooFestBooking", { 
         method: "POST",
         body: bodyContent,
         headers: headersList
       });
 
       if (response.status === 201) {
-        router.push('/tickets');
+        router.push("/orderConfirmation"); // Redirect to a success page or another route after successful submission
       } else {
         console.error('Failed to submit:', await response.text());
       }
@@ -63,10 +78,10 @@ export default function GuestForm() {
         <h2 className="mb-4">Guest 1</h2>
         <label>
           <span>Country</span>
-          <select 
+          <select
             className="rounded-md p-3 border-2 w-60 hover:bg-White active:bg-White focus:outline-none focus:ring focus:ring-Hotpink text-Darkblue"
             onChange={(e) => setCountry(e.target.value)}
-            value={country} 
+            value={country}
           >
             <option value="Denmark">Denmark</option>
             <option value="Sweden">Sweden</option>
@@ -76,93 +91,93 @@ export default function GuestForm() {
         <div>
           <label>
             <span>First name</span>
-            <input 
-              className="rounded-md p-2 border-2 hover:bg-White active:bg-White focus:outline-none focus:ring focus:ring-Hotpink text-Darkblue" 
+            <input
+              className="rounded-md p-2 border-2 hover:bg-White active:bg-White focus:outline-none focus:ring focus:ring-Hotpink text-Darkblue"
               required
               type="text"
               placeholder="Jane"
               name="firstname"
               onChange={(e) => setFirstName(e.target.value)}
-              value={firstname} 
+              value={firstname}
             />
           </label>
           <label>
             <span>Last name</span>
-            <input 
-              className="rounded-md p-2 border-2 hover:bg-White active:bg-White focus:outline-none focus:ring focus:ring-Hotpink text-Darkblue" 
+            <input
+              className="rounded-md p-2 border-2 hover:bg-White active:bg-White focus:outline-none focus:ring focus:ring-Hotpink text-Darkblue"
               required
               type="text"
               placeholder="Doe"
               name="lastname"
               onChange={(e) => setLastName(e.target.value)}
-              value={lastname} 
+              value={lastname}
             />
           </label>
         </div>
         <label>
           <span>Email</span>
-          <input 
-            className="rounded-md p-2 border-2 hover:bg-White active:bg-White focus:outline-none focus:ring focus:ring-Hotpink text-Darkblue" 
+          <input
+            className="rounded-md p-2 border-2 hover:bg-White active:bg-White focus:outline-none focus:ring focus:ring-Hotpink text-Darkblue"
             required
             type="email"
             placeholder="example@gmail.com"
             name="email"
             onChange={(e) => setEmail(e.target.value)}
-            value={useremail} 
+            value={useremail}
           />
         </label>
         <label>
           <span>Postal code</span>
-          <input 
-            className="rounded-md p-2 border-2 hover:bg-White active:bg-White focus:outline-none focus:ring focus:ring-Hotpink text-Darkblue" 
+          <input
+            className="rounded-md p-2 border-2 hover:bg-White active:bg-White focus:outline-none focus:ring focus:ring-Hotpink text-Darkblue"
             required
             type="text"
             placeholder="2950"
             name="postal"
             maxLength="4"
             onChange={(e) => setPostal(e.target.value)}
-            value={postalcode} 
+            value={postalcode}
           />
         </label>
         <label>
           <span>Address</span>
-          <input 
-            className="rounded-md p-2 border-2 hover:bg-White active:bg-White focus:outline-none focus:ring focus:ring-Hotpink text-Darkblue" 
+          <input
+            className="rounded-md p-2 border-2 hover:bg-White active:bg-White focus:outline-none focus:ring focus:ring-Hotpink text-Darkblue"
             required
             type="text"
             placeholder="Address"
             name="address"
             onChange={(e) => setAddress(e.target.value)}
-            value={address} 
+            value={address}
           />
         </label>
         <label>
           <span>City</span>
-          <input 
-            className="rounded-md p-2 border-2 hover:bg-White active:bg-White focus:outline-none focus:ring focus:ring-Hotpink text-Darkblue" 
+          <input
+            className="rounded-md p-2 border-2 hover:bg-White active:bg-White focus:outline-none focus:ring focus:ring-Hotpink text-Darkblue"
             required
             type="text"
             placeholder="Paris"
             name="city"
             onChange={(e) => setCity(e.target.value)}
-            value={city} 
+            value={city}
           />
         </label>
         <label>
           <span>Phone number</span>
-          <input 
-            className="rounded-md p-2 border-2 hover:bg-White active:bg-White focus:outline-none focus:ring focus:ring-Hotpink text-Darkblue" 
+          <input
+            className="rounded-md p-2 border-2 hover:bg-White active:bg-White focus:outline-none focus:ring focus:ring-Hotpink text-Darkblue"
             required
             type="text"
             placeholder="+45 12 34 56 78"
             name="phonenumber"
             onChange={(e) => setPhone(e.target.value)}
-            value={phonenumber} 
+            value={phonenumber}
           />
         </label>
-        <button 
-          type="submit" 
-          className="text-center text-White p-2 mt-6 rounded-lg bg-Hotpink hover:bg-Lightoink w-60"
+        <button
+          type="submit"
+          className="text-center text-White p-2 mt-6 rounded-lg bg-Hotpink hover:bg-Lightpink w-60"
           disabled={isLoading}
         >
           {isLoading ? 'Submitting...' : 'Submit Information'}
