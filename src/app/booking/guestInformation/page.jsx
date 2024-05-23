5f5c92c18ae6f81636e907d64d4de392b60500d7
Frederikke <frny0001@stud.kea.dk>
2024-05-23 09:55:08 +0200

    ændring

--- src/app/booking/guestInformation/page.jsx
+++ src/app/booking/guestInformation/page.jsx
@@ -12,7 +12,7 @@ export default function Formpage() {
   return (
     <main>
  <div className="text-White bg-Darkblue p-12 m-16 mt-20 rounded-md border-2 border-Hotpink" >
- <h1 className={` text-White text-4xl m-4 ${BowlbyOne.className}`}>INFORMATION</h1>
+ <h1 className={` text-White text-4xl mb-8 ${BowlbyOne.className}`}>INFORMATION</h1>
       <div className="grid grid-cols-2">
       <div>
     <GuestForm/>
