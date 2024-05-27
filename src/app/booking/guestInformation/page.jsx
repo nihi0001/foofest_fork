999a7288e143e150248a47c3180af44111321018
Frederikke <frny0001@stud.kea.dk>
2024-05-27 21:01:49 +0200

    responsivGuestInfo

--- src/app/booking/guestInformation/page.jsx
+++ src/app/booking/guestInformation/page.jsx
@@ -19,21 +19,24 @@ export default function Formpage() {
   //const area = searchParams.get('area ')
   return (
     <main>
- <div className="text-White bg-Darkblue p-12 m-4 md:p-12 md:m-16 mt-20 rounded-md border-2 border-Hotpink" >
- <h1 className={` text-White text-2xl md:text-4xl mb-8 ${BowlbyOne.className}`}>INFORMATION</h1>
-      <div className="md:grid grid-cols-2">
-      <div>
-        
-    <GuestForm/>
-    </div>
-    <div>
-      <GuestInformation />
+      <div className=" text-White bg-darkblue p-4 sm:p-8 md:p-12 m-2 sm:m-4 md:m-20 mt-10 sm:mt-12 md:mt-20 rounded-md border-2 border-Hotpink">
+        <h1 className={`text-White text-3xl md:text-4xl mb-6 sm:mb-6 md:mb-8 ${BowlbyOne.className}`}>
+          INFORMATION
+        </h1>
+        <div className=" grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8">
+          <div className=" w-full">
+            <div className="w-full max-w-sm mb-10 md:mb-2">
+              <GuestForm />
+            </div>
+          </div>
+          <div className="flex justify-center w-full">
+            <div className="w-full max-w-sm mb-6 md:mb-2">
+              <GuestInformation />
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-    </div>
-   
-  </main>
-  )
+    </main>
+  );
 }
-
 
