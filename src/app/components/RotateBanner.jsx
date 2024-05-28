fc35aadf7aa61dbb3f7f8523935de33f212f61b2
Nina <nihi0001@kea.dk>
2024-05-28 10:26:52 +0200

    styling

--- src/app/components/RotateBanner.jsx
+++ src/app/components/RotateBanner.jsx
@@ -9,8 +9,8 @@ const BowlbyOne = Bowlby_One({
 
 function RotateBanner() {
     return (
-        <div className="bg-Hotpink text-xl relative flex overflow-x-hidden font-medium mt-12">
-            <div className={`py-4 animate-marquee whitespace-nowrap text-White ${BowlbyOne.className}`}>
+        <div className="bg-Hotpink text-lg sm:text-xl relative flex overflow-x-hidden font-medium mt-12">
+            <div className={`py-3 sm:py-4 animate-marquee whitespace-nowrap text-White ${BowlbyOne.className}`}>
                 <span className="mx-16">FOO FESTIVAL</span>
                 <span className="mx-16">JULY 3RD - 10TH 2024</span>
                 <span className="mx-16">TECHNO</span>
