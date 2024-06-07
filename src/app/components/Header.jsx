be28f7c4a8be826a32658c6b694950dab181d759
Nina <nihi0001@kea.dk>
2024-06-07 21:06:51 +0200

    navbar og kommentare

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -3,6 +3,8 @@
 import Link from 'next/link';
 import { useState } from 'react';
 import { Bowlby_One } from 'next/font/google';
+import RotateBanner from "./RotateBanner";
+
 
 const BowlbyOne = Bowlby_One({
   subsets: ['latin'],
@@ -10,8 +12,11 @@ const BowlbyOne = Bowlby_One({
   display: 'swap',
 });
 
+/* menuOpen er sat til false, da den først skal åbne når man trykker på kanppen*/
 export default function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
+
+  /* toggleMenu skifter menuOpen til true eller false  */
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
@@ -19,30 +24,35 @@ export default function Header() {
 
   return (
     <>
+      {/* pink banner */}
+
       <div className="bg-Hotpink py-2">
         <p className="text-xs sm:text-sm 2xl:text-xl text-White text-center font-medium tracking-wider">
           Camping Spots Still Available - Reserve Yours Today!
         </p>
       </div>
 
-      <nav className="sticky top-0 z-50 bg-Darkblue text-White px-4 py-4 2xl:py-5 flex justify-between items-center md:px-10">
-  <div>
-    <Link
-      className={`font-bold text-xl sm:text-2xl 2xl:text-xl ${BowlbyOne.className} hover:text-Hotpink`}
-      href="/"
-      prefetch={false}
-    >
-      FOOFEST
-    </Link>
-    <p className="text-md text-White font-normal">July 3rd - July 10th 2024</p>
-  </div>
+      {/* nav bar */}
 
+      <nav className="sticky top-0 z-50 bg-Darkblue text-White px-4 py-4 2xl:py-5 flex justify-between items-center md:px-10">
+        <div>
+          <Link
+            className={`font-bold text-xl sm:text-2xl 2xl:text-xl ${BowlbyOne.className} hover:text-Hotpink`}
+            href="/"
+            prefetch={false}
+          >
+            FOOFEST
+          </Link>
+          <p className="text-md text-White font-normal">July 3rd - July 10th 2024</p>
+        </div>
+
+        {/* billede af burger menu symbol - åben*/}
         <button
           className="text-White sm:hidden"
           onClick={toggleMenu}
         >
           <svg
-            className="w-6 h-6"
+            className="w-8 h-8"
             fill="none"
             stroke="currentColor"
             viewBox="0 0 24 24"
@@ -52,6 +62,8 @@ export default function Header() {
           </svg>
         </button>
 
+        {/* de tre links til højre i web størrelse */}
+        {/* skjult (hidden) på små skærme, men synlige på større skærme */}
         <ul className="hidden sm:flex items-center gap-4 md:gap-10">
           <li>
             <Link
@@ -83,6 +95,10 @@ export default function Header() {
         </ul>
       </nav>
 
+      {/* burger menu */}
+      {/* bliver synlig når menuOpen er sat til true */}
+      {/* Når menuOpen er true er menuen synlig med translate-x-0. Når mmenuOpen er false bruges translate-x-full og burgermenuen er skjult   */}
+
       <div className={`fixed inset-0 bg-Darkblue text-White transition-transform transform ${menuOpen ? 'translate-x-0' : 'translate-x-full'} z-50 sm:hidden flex flex-col`}>
         <div className="bg-Hotpink py-2">
           <p className="text-xs sm:text-sm 2xl:text-xl text-White text-center font-medium tracking-wider">
@@ -90,22 +106,25 @@ export default function Header() {
           </p>
         </div>
         <div className="flex justify-between items-center p-4">
-           <div>
-    <Link
-      className={`font-bold text-xl sm:text-2xl 2xl:text-2xl ${BowlbyOne.className} hover:text-Hotpink`}
-      href="/"
-      prefetch={false}
-    >
-      FOOFEST
-    </Link>
-    <p className="text-md text-White font-regular">July 3rd - July 10th 2024</p>
-  </div>
+          <div>
+            <Link
+              className={`font-bold text-xl sm:text-2xl 2xl:text-2xl ${BowlbyOne.className} hover:text-Hotpink`}
+              href="/"
+              prefetch={false}
+            >
+              FOOFEST
+            </Link>
+            <p className="text-md text-White font-regular">July 3rd - July 10th 2024</p>
+          </div>
+
+          {/* billede af burger menu symbol - luk */}
+
           <button
             className="text-White"
             onClick={toggleMenu}
           >
             <svg
-              className="w-6 h-6"
+              className="w-8 h-8"
               fill="none"
               stroke="currentColor"
               viewBox="0 0 24 24"
@@ -115,12 +134,15 @@ export default function Header() {
             </svg>
           </button>
         </div>
-        <ul className="flex flex-col justify-center items-center h-full space-y-12 overflow-y-auto bg-Navyblue">
+
+        {/* burger menu elementer */}
+
+        <ul className="flex flex-col items-center h-full space-y-12 overflow-y-auto bg-Navyblue pt-32">
           <li>
             <Link
               href="/booking"
               prefetch={false}
-              className="bg-Hotpink font-bold text-xl rounded-full py-2 px-6 hover:bg-Lightpink"
+              className="bg-Hotpink font-bold text-2xl rounded-full py-2 px-6 hover:bg-Lightpink"
               onClick={toggleMenu}
             >
               BUY TICKETS
@@ -128,7 +150,7 @@ export default function Header() {
           </li>
           <li>
             <Link
-              className="font-bold text-xl hover:text-Hotpink"
+              className="font-bold text-2xl hover:text-Hotpink"
               href="/lineup"
               prefetch={false}
               onClick={toggleMenu}
@@ -138,7 +160,7 @@ export default function Header() {
           </li>
           <li>
             <Link
-              className="font-bold text-xl hover:text-Hotpink"
+              className="font-bold text-2xl hover:text-Hotpink"
               href="/schedule"
               prefetch={false}
               onClick={toggleMenu}
@@ -146,7 +168,11 @@ export default function Header() {
               SCHEDULE
             </Link>
           </li>
+          <div className='pt-32'>
+            <RotateBanner />
+          </div>
         </ul>
+
       </div>
     </>
   );
