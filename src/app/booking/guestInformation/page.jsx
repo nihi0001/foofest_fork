b00320b68a4924daa5ede7e2993044aae6756c64
Nina <nihi0001@kea.dk>
2024-06-08 14:03:38 +0200

    info

--- src/app/booking/guestInformation/page.jsx
+++ src/app/booking/guestInformation/page.jsx
@@ -14,9 +14,8 @@ export default function Formpage() {
   return (
     <main>
       <div className="text-White bg-Darkblue p-4 sm:p-6 md:p-12 lg:p-16 m-4 sm:m-4 md:m-12 lg:mx-44 rounded-3xl border-2 border-Hotpink">
-        <h1 className={`text-White text-3xl md:text-4xl mb-4 sm:mb-6 md:mb-8  ${BowlbyOne.className}`}>
-          INFORMATION
-        </h1>
+      <h1 className={`${BowlbyOne.className} text-White text-4xl sm:text-6xl mb-5 md:p-8 text-start sm:text-center`}>INFORMATION</h1>
+
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 sm:gap-6 md:gap-8 lg:gap-10 xl:gap-12">
           <div className="w-full">
             <div className="w-full mb-6 md:mb-4 lg:mb-2">
