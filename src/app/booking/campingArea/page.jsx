09cf1ce7feb2f719e2b35c657be0b0abe8202b6a
Nina <nihi0001@kea.dk>
2024-06-08 13:59:57 +0200

    styling

--- src/app/booking/campingArea/page.jsx
+++ src/app/booking/campingArea/page.jsx
@@ -8,29 +8,26 @@ import RemoveTent from '@/app/components/Buttons/RemoveTent';
 import AddTent from '@/app/components/Buttons/AddTent';
 import ChooseTicket from '@/app/components/ChooseTicket';
 
-
 const BowlbyOne = Bowlby_One({
   subsets: ["latin"],
   weight: "400",
   display: "swap",
 });
 
-
 export default function Home() {
-   const searchParams = useSearchParams()
-   const regular = searchParams.get('ticketcount1')
-   const vip = searchParams.get('ticketcount2')
+  const searchParams = useSearchParams();
+  const regular = searchParams.get('ticketcount1');
+  const vip = searchParams.get('ticketcount2');
 
   // + laver det om til et tal fremfor en string
   const [twoTentTicket, setTwoTentTicket] = useState(0);
   const [threeTentTicket, setThreeTentTicket] = useState(0);
   const [campingArea, setCampingArea] = useState('');
 
-
   const handleAddTent = (count) => {
-    setTwoTentTicket(count);  
+    setTwoTentTicket(count);
   };
-  
+
   const handleRemoveTent = () => {
     if (twoTentTicket > 0) {
       setTwoTentTicket(prevCount => prevCount - 1);
@@ -40,7 +37,7 @@ export default function Home() {
   const handleAddVipTent = (count) => {
     setThreeTentTicket(count);
   };
-  
+
   const handleRemoveVipTent = () => {
     if (threeTentTicket > 0) {
       setThreeTentTicket(prevCount => prevCount - 1);
@@ -48,94 +45,93 @@ export default function Home() {
   };
 
   const handleCampingArea = (e) => {
-    if(e.target.checked){
+    if (e.target.checked) {
       setCampingArea(e.target.value)
     } else setCampingArea('')
-    
-  }
-
-
+  };
 
   return (
     <>
-    <form action="/booking/guestInformation">
-      <input type="hidden" name="ticketcount1" value={regular}/>
-      <input type="hidden" name="ticketcount2" value={vip}/>
+      <form action="/booking/guestInformation">
+        <input type="hidden" name="ticketcount1" value={regular} />
+        <input type="hidden" name="ticketcount2" value={vip} />
 
-    <div className="w-11/12 p-3 border-solid bg-Darkblue border-Hotpink border-2 rounded-3xl xl:p-20 lg:w-9/12 m-auto mt-10">
-      <h1 className={`text-White text-2xl mb-10 md:mb-0 lg:text-4xl ${BowlbyOne.className}`}>CAMPING AREA</h1>
-        <div className="grid gap-10 xl:flex xl:justify-between xl:py-8">
+        <div className='p-4'>
+          <div className="max-w-6xl mx-auto border-solid bg-Darkblue border-Hotpink border-2 rounded-3xl p-8 mt-10">
+            <h1 className={`${BowlbyOne.className} text-White text-4xl sm:text-6xl mb-5 md:p-8 text-start sm:text-center`}>CAMPING AREA</h1>
+            <div className="grid sm:grid-cols-2 gap-10">
 
-          <div className=''> 
-            <h2 className='text-White sm:text-base xl:text-2xl font-bold mb-5'>CHOOSE YOUR CAMPING AREA</h2>
-            <div className='grid xl:grid-cols-2 text-White'>
-            <label className='hover:text-Hotpink text-xl mt-2'><input className='w-5 h-5' defaultValue="Svartheim" onChange={handleCampingArea} type="radio" name='area'  /> Svartheim</label>
-            <label className='hover:text-Hotpink text-xl mt-2'><input className='w-5 h-5' defaultValue="Nilfheim" onChange={handleCampingArea} type="radio" name='area' /> Nilfheim</label>
-            <label className='hover:text-Hotpink text-xl mt-2'><input className='w-5 h-5' defaultValue="Helheim" onChange={handleCampingArea} type="radio" name='area' /> Helheim</label>
-            <label className='hover:text-Hotpink text-xl mt-2'><input className='w-5 h-5' defaultValue="Muspelheim" onChange={handleCampingArea} type="radio" name='area' /> Muspelheim</label>
-            <label className='hover:text-Hotpink text-xl mt-2'><input className='w-5 h-5' defaultValue="Alfheim" onChange={handleCampingArea} type="radio" name='area' /> Alfheim</label>
+              <div className="md:p-8">
+                <h2 className='mb-8 text-xl xl:text-2xl text-White font-semibold'>Choose your camping area</h2>
+                <div className='grid xl:grid-cols-2 text-White'>
+                  <label className='hover:text-Hotpink text-xl mt-2'><input className='w-5 h-5' defaultValue="Svartheim" onChange={handleCampingArea} type="radio" name='area' /> Svartheim</label>
+                  <label className='hover:text-Hotpink text-xl mt-2'><input className='w-5 h-5' defaultValue="Nilfheim" onChange={handleCampingArea} type="radio" name='area' /> Nilfheim</label>
+                  <label className='hover:text-Hotpink text-xl mt-2'><input className='w-5 h-5' defaultValue="Helheim" onChange={handleCampingArea} type="radio" name='area' /> Helheim</label>
+                  <label className='hover:text-Hotpink text-xl mt-2'><input className='w-5 h-5' defaultValue="Muspelheim" onChange={handleCampingArea} type="radio" name='area' /> Muspelheim</label>
+                  <label className='hover:text-Hotpink text-xl mt-2'><input className='w-5 h-5' defaultValue="Alfheim" onChange={handleCampingArea} type="radio" name='area' /> Alfheim</label>
+                </div>
+                <br />
+
+                <h2 className='text-White text-xl xl:text-2xl mb-8 mt-12 font-semibold'>Choose your tents</h2>
+
+                <div className='grid 2xl:flex 2xl:gap-24'>
+                  <div className='text-White'>
+                    <h2 className="text-xl text-White"><span className='font-bold text-Hotpink'>2</span> PERSON TENT</h2>
+                    <p>299 DKK</p>
+                  </div>
+
+                  <div className='flex flex-row gap-3 h-6 items-center mt-4 mb-5 2xl:ml-4'>
+                    <RemoveTent onRemoveTentClick={handleRemoveTent} />
+                    <input
+                      name="twoTentTicket"
+                      value={twoTentTicket}
+                      className='border-solid border-2 border-Lightpink rounded-full w-32 bg-White p-2 text-center' />
+                    <AddTent onAddTentClick={() => handleAddTent(twoTentTicket + 1)} />
+                  </div>
+                </div>
+
+                <br />
+                <div className='grid 2xl:flex 2xl:gap-28'>
+                  <div>
+                    <h2 className='text-White text-xl'><span className='font-bold text-Hotpink'> 3 </span>PERSON TENT</h2>
+                    <p className='text-White text-base'>399 DKK</p>
+                  </div>
+
+                  <div className='flex flex-row gap-3 h-6 items-center mt-4'>
+                    <RemoveTent onRemoveTentClick={handleRemoveVipTent} />
+                    <input
+                      name="threeTentTicket"
+                      value={threeTentTicket}
+                      className='border-solid border-2 border-Lightpink rounded-full w-32 bg-White p-2 text-center'
+                    />
+                    <AddTent onAddTentClick={() => handleAddVipTent(threeTentTicket + 1)} />
+                  </div>
+                </div>
+                <br />
+              </div>
+
+              <div className='grid gap-5'>
+                <div className='m-auto'>
+                  <CampingTickets
+                    tentTicket={twoTentTicket}
+                    handleAddTent={handleAddTent}
+                    handleRemoveTent={handleRemoveTent}
+                    tentPrice={299}
+                    threeTentTicket={threeTentTicket}
+                    handleAddVipTent={handleAddVipTent}
+                    handleRemoveVipTent={handleRemoveVipTent}
+                    threeTentPrice={399}
+                    campingArea={campingArea}
+                  />
+                </div>
+                <div className='m-auto'>
+                  <ChooseTicket />
+                </div>
+              </div>
             </div>
-            <br />  
-
-            <h2 className='text-White text-2xl mb-5 mt-10 font-bold'>TENTS</h2>
-
-            <div className='grid 2xl:flex 2xl:gap-24'>
-              <div className='text-White'>
-                <h2 className="text-xl text-White"><span className='font-bold text-Hotpink'>2</span> PERSON TENT</h2>
-                <p>299 DKK</p>
-              </div>
-
-              <div className='flex flex-row gap-3 h-6 items-center mt-4 mb-5 2xl:ml-4'>
-                <RemoveTent onRemoveTentClick={handleRemoveTent} /> 
-                <input 
-                name="twoTentTicket" 
-                value={twoTentTicket}
-                className='border-solid border-2 border-Lightpink rounded-full w-32 bg-White p-2' />
-                <AddTent onAddTentClick={() => handleAddTent(twoTentTicket + 1)} />
-              </div>
-            </div>
-
-            <br />
-            <div className='grid 2xl:flex 2xl:gap-28'>
-              <div>
-                <h2 className='text-White text-xl'><span className='font-bold text-Hotpink'> 3 </span>PERSON TENT</h2>
-                <p className='text-White text-base'>399 DKK</p>
-              </div>
-
-              <div className='flex flex-row gap-3 h-6 items-center mt-4'>
-                <RemoveTent onRemoveTentClick={handleRemoveVipTent} /> 
-                <input 
-                name="threeTentTicket" 
-                value={threeTentTicket}
-                className='border-solid border-2 border-Lightpink rounded-full w-32 bg-White p-2' 
-                 />
-                <AddTent onAddTentClick={() => handleAddVipTent(threeTentTicket + 1)} />
-              </div>
-            </div>
-            <br />
-          </div>
-
-          <div className='grid gap-5'>
-          <div className='m-auto xl:mr-44'> 
-            <CampingTickets 
-            tentTicket={twoTentTicket} 
-            handleAddTent={handleAddTent} 
-            handleRemoveTent={handleRemoveTent} 
-            tentPrice={299}
-            threeTentTicket={threeTentTicket} 
-             handleAddVipTent={handleAddVipTent} 
-             handleRemoveVipTent={handleRemoveVipTent} 
-             threeTentPrice={399} 
-             campingArea={campingArea}
-             />  
-          </div>
-          <div className='m-auto xl:ml-32'>
-            <ChooseTicket />
-          </div>
           </div>
         </div>
-    </div>
-    </form>
+      </form>
     </>
   );
 }
--- src/app/booking/ticketoverview/page.jsx
+++ src/app/booking/ticketoverview/page.jsx
@@ -22,7 +22,7 @@ export default function Home() {
   const handleTicketChange = (count) => {
     setTicketcount1(count);
   };
-  
+
   const handleRemoveTicket = () => {
     if (ticketcount1 > 0) {
       setTicketcount1(prevCount => prevCount - 1);
@@ -32,75 +32,77 @@ export default function Home() {
   const handleTicketChange2 = (count) => {
     setTicketcount2(count);
   };
-  
+
   const handleRemoveTicket2 = () => {
     if (ticketcount2 > 0) {
       setTicketcount2(prevCount => prevCount - 1);
     }
   };
-  
+
 
   return (
     <>
-    <form action='/booking/campingArea'>
-    <div className="sm:w-11/12 border-solid bg-Darkblue border-Hotpink border-2 rounded-3xl p-8 lg:w-7/12 m-auto mt-10">
-      <h1 className={` ${BowlbyOne.className} text-White text-2xl lg:text-4xl mb-5`}>TICKETS</h1>
-        <div className="grid md:flex md:p-8 gap-28">
-    
-          <div>
-            <h2 className='mt-8 mb-5 sm:text-base xl:text-2xl md:mt-0 text-White'>CHOOSE YOUR TICKETS</h2>
-            <div className='grid mb-5 2xl:flex 2xl:gap-24'> 
-              <div className='text-White'>
-                <h2 className="text-xl text-White"><span className='text-Hotpink font-bold'>FOO</span> TICKET</h2>
-                <p>799 DKK</p>
-              </div>
-              <div className='flex flex-row gap-3 h-6 items-center mt-4'>
-                <RemoveTicket onRemoveButtonClick={handleRemoveTicket} /> 
-                <input 
-                name="ticketcount1" 
-                className='border-solid border-2 border-Lightpink rounded-full w-32 bg-White p-2' 
-                value={ticketcount1} />
-                <AddButton onButtonClick={() => handleTicketChange(ticketcount1 + 1)} />
-              </div>
-            </div>
+      <form action='/booking/campingArea'>
+        <div className='p-4'>
+          <div className="max-w-6xl mx-auto border-solid bg-Darkblue border-Hotpink border-2 rounded-3xl p-8 mt-10">
+          <h1 className={`${BowlbyOne.className} text-White text-4xl sm:text-6xl mb-5 md:p-8 text-start sm:text-center`}>TICKETS</h1>
+            <div className="grid md:flex md:p-8 gap-28">
 
-            <br />
-            <div className='grid 2xl:flex 2xl:gap-28'>
               <div>
-                <h2 className='text-White text-xl'><span className='font-bold text-Hotpink'>VIP</span> TICKET</h2>
-                <p className='text-White text-base'>1299 DKK</p>
+                <h2 className='mb-8 text-xl xl:text-2xl text-White font-semibold'>Choose your tickets:</h2>
+                <div className='grid mb-5 2xl:flex 2xl:gap-24'>
+                  <div className='text-White'>
+                    <h2 className="text-xl text-White"><span className='text-Hotpink font-bold'>FOO</span> TICKET</h2>
+                    <p>799 DKK</p>
+                  </div>
+                  <div className='flex flex-row gap-3 h-6 items-center mt-4'>
+                    <RemoveTicket onRemoveButtonClick={handleRemoveTicket} />
+                    <input
+                      name="ticketcount1"
+                      className='border-solid border-2 border-Lightpink rounded-full w-32 bg-White p-2'
+                      value={ticketcount1} />
+                    <AddButton onButtonClick={() => handleTicketChange(ticketcount1 + 1)} />
+                  </div>
+                </div>
+
+                <br />
+                <div className='grid 2xl:flex 2xl:gap-28'>
+                  <div>
+                    <h2 className='text-White text-xl'><span className='font-bold text-Hotpink'>VIP</span> TICKET</h2>
+                    <p className='text-White text-base'>1299 DKK</p>
+                  </div>
+
+                  <div className='flex flex-row gap-3 h-6 items-center mt-4'>
+                    <RemoveTicket onRemoveButtonClick={handleRemoveTicket2} />
+                    <input
+                      name="ticketcount2"
+                      className='border-solid border-2 border-Lightpink rounded-full w-32 bg-White p-2'
+                      value={ticketcount2} />
+                    <AddButton onButtonClick={() => handleTicketChange2(ticketcount2 + 1)} />
+                  </div>
+                </div>
               </div>
 
-              <div className='flex flex-row gap-3 h-6 items-center mt-4'>
-                <RemoveTicket onRemoveButtonClick={handleRemoveTicket2} /> 
-                <input 
-                name="ticketcount2" 
-                className='border-solid border-2 border-Lightpink rounded-full w-32 bg-White p-2' 
-                value={ticketcount2} />
-                <AddButton onButtonClick={() => handleTicketChange2(ticketcount2 + 1)} />
+              <div className='grid gap-5 m-auto'>
+                <div>
+                  <Ticket
+                    ticketcount1={ticketcount1}
+                    handleTicketChange={handleTicketChange}
+                    handleRemoveTicket={handleRemoveTicket}
+                    ticketPrice={799}
+                    ticketcount2={ticketcount2}
+                    handleTicketChange2={handleTicketChange2}
+                    handleRemoveTicket2={handleRemoveTicket2}
+                    ticketPrice2={1299} />
+                </div>
+                <div className='m-auto'>
+                  <ChooseTicket />
+                </div>
               </div>
             </div>
-          </div>
-
-          <div className='grid gap-5 m-auto'>
-          <div>
-            <Ticket 
-            ticketcount1={ticketcount1} 
-            handleTicketChange={handleTicketChange} 
-            handleRemoveTicket={handleRemoveTicket} 
-            ticketPrice={799}
-             ticketcount2={ticketcount2} 
-             handleTicketChange2={handleTicketChange2} 
-             handleRemoveTicket2={handleRemoveTicket2} 
-             ticketPrice2={1299} />
-          </div>
-          <div className='m-auto'>
-            <ChooseTicket />
-          </div>
           </div>
         </div>
-    </div>
-    </form>
+      </form>
     </>
   );
 }
